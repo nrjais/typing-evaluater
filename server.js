5ad5317dd987f957d3d165d93fb676205398a0be
Neeraj Jaiswal <neerajj85@gmail.com>
2018-01-08 18:36:54 +0530

    Moved server to webapp framework

--- server.js
+++ server.js
@@ -1,61 +1,55 @@
 const http = require('http');
 const fs = require('fs');
-const qs = require('querystring');
-const texts = require('./data/texts.json');
+let texts = [];
+const webApp = require('./webapp');
 let PORT = 8888;
 
-const handleFileRequest = function (fileName, res, onFile, onError) {
-  fileName = 'public' + fileName;
-  fs.access(fileName, (err) => {
-    err ? onError(res) : onFile(fileName, res);
+const app = webApp();
+
+let loadTexts = function () {
+  fs.readFile('./data/texts.json', (err, data) => {
+    if (err) texts = [];
+    else texts = JSON.parse(data);
   });
 }
+loadTexts();
 
 const getHeader = function (fileName) {
   let extension = fileName.slice(fileName.lastIndexOf('.'));
   let headers = {
     '.css': 'text/css',
     '.html': 'text/html',
-    '.js': 'text/javascript',
-    '.jpg': 'image/jpg',
-    '.gif': 'image/gif',
-    '.pdf': 'application/pdf'
+    '.js': 'text/javascript'
   }
   return headers[extension];
 }
 
-const saveText = function (text) {
-  let data = qs.parse(text);
+const saveText = function (req, res) {
+  let data = req.body;
   data.date = new Date().toLocaleString()
   texts.push(data);
-  fs.writeFile('data/texts.json', JSON.stringify(texts,null,2), logError);
+  fs.writeFile('data/texts.json', JSON.stringify(texts, null, 2), logError);
+  res.redirect('/index.html');
 };
 
-const addNewText = function (req, res) {
-  let postData = '';
-  req.on('data', (data) => {
-    postData += data;
-  });
-  req.on('end', () => {
-    saveText(postData);
-    redirectTo('index.html', res);
-  });
-};
-
-const getRandomText = function (req,res) {
+const getRandomText = function (req, res) {
   let randomIndex = Math.floor(Math.random() * texts.length);
   let text = `var text = "${texts[randomIndex].text.trim()}";`;
+  res.setHeader('content-type', 'text/javascript');
   res.write(text);
   res.end();
 };
 
-const readFileContent = function (fileName, res) {
-  setHeader(res, fileName);
-  fs.readFile(fileName, (err, data) => {
-    if (!err)
-      res.write(data);
+const serveFile = function (req, res) {
+  try {
+    fileName = 'public' + req.url;
+    let data = fs.readFileSync(fileName);
+    setHeader(res, fileName);
+    res.write(data);
     res.end();
-  });
+  } catch (e) {
+    return;
+  }
 }
 
 const setHeader = function (res, fileName) {
@@ -69,43 +63,17 @@ const logError = function (err) {
     console.log(err);
 }
 
-const redirectTo = function (location, res) {
-  res.statusCode = 302;
-  res.setHeader('Location', location);
-  res.end();
-}
-
-const logger = function (req) {
+const logRequest = function (req) {
   const time = new Date().toLocaleString();
   console.log(`${time} ${req.method} ${req.url}`);
 }
 
-const fileNotFoundError = function (res) {
-  res.statusCode = 404;
-  res.write("The file you are looking for is not present on this server");
-  res.end();
-}
+app.use(logRequest);
+app.use(serveFile);
+app.get('/', (req, res) => res.redirect('index.html'));
+app.get('/text', getRandomText);
+app.post('/addText', saveText);
 
-const handlers = {
-  '/': (req, res) => {
-    redirectTo('index.html', res);
-  },
-  '/addText': addNewText,
-  '/text': getRandomText
-}
-
-const requestHandler = function (req, res) {
-  logger(req);
-  let url = req.url;
-
-  let handler = handlers[url];
-  if (handler) {
-    handler(req, res);
-    return
-  }
-  handleFileRequest(url, res, readFileContent, fileNotFoundError);
-};
-
-const server = http.createServer(requestHandler);
+const server = http.createServer(app);
 server.listen(PORT);
 console.log('Listening on port ' + PORT);
